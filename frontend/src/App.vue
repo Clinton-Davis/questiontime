1de4b84f2d8adcc045daa334eaebc9082a3f4f5b
Clinton-Dais <clintongadavis@gmail.com>
2022-01-10 19:44:53 +0000

    added navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,24 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Navbar />
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+};
+</script>
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  font-family: "Noto Sans JP", sans-serif;
+  font-weight: 300;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.btn:focus {
+  box-shadow: none !important;
 }
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark my-navbar">
+    <div class="container">
+      <router-link class="navbar-brand" :to="{ name: 'home' }"
+        >Questions Time</router-link
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="/accounts/logout/" class="btn btn-outline-primary"
+              >Logout</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbar-brand {
+  font-weight: bold;
+  font-size: 1.7rem;
+  -webkit-text-stroke: 0.001rem rgb(57, 4, 247);
+}
+.navbar-brand:hover,
+.navbar-brand:active {
+  -webkit-text-stroke: 0.001rem rgb(255, 255, 255);
+  color: #fd0019;
+}
+</style>
